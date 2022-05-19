749fb5de464b8c680d156c7d6864dace9226d6ac
Alexis <alexisjlino@gmail.com>
2022-05-18 22:00:46 -0300

    Sigue sin andar el pipe ä

--- Pipes.py
+++ Pipes.py
@@ -10,34 +10,40 @@ def Main():
 
 
 def Pipes(args):
-    r, w = os.pipe()
-    fd1 = open(args.f, "r+")
+    r1, w1 = os.pipe()
+    r2, w2 = os.pipe()
+    fd1 = open(args.f, "r")
     for i in fd1.readlines():
-        w = os.fdopen(w, 'w')
-        w.write(i)
         
         ret = os.fork()
         if ret == 0:
-            w.close()
-            r = os.fdopen(r)
+            os.close(w1)
+            r1 = os.fdopen(r1, 'r')
             print("Hijo leyendo...", os.getpid())
-            texto = r.readline()
-            r.close()
-            w = os.fdopen(w, 'w')
+            texto = r1.readline()
+            r1.close()
+            w2 = os.fdopen(w2, 'w')
             texto = texto[::-1]
-            w.write(texto)
+            w2.write(texto)
             os._exit(0)
         else:
-            w.close()
-            time.sleep(5)
-            r = os.fdopen(r)
-            p_lectura = r.readline()
+            os.close(r1)
+            w1 = os.fdopen(w1, 'w')
+            w1.write(i)
+            w1.close()
+            #Padre lee
+            time.sleep(1)
+            os.close(w2)
+            r2 = os.fdopen(r2, 'r')
+            p_lectura = r2.readline()
             print(p_lectura)
-             
-    
-    for g in fd1.readline():
-        print(g)
-        os.wait()
+            r2.close()
+            os.wait()
+            sys.exit(0)
+
+    # for g in fd1.readline():
+    #     print(g)
+    #     os.wait()
     fd1.close()
 
 def path_search(args):
@@ -45,9 +51,15 @@ def path_search(args):
     if os.path.exists(args.f):
         pass
     else:
-        print("Se creará el archivo especificado en la ruta.")
+        print("Se creará el archivo especificado en la ruta y le escribirá un"
+        "texto de ejemplo")
         archive = open(args.f, "w")
         archive.close()
-
+        fd = open(args.f, "a")
+        with open(args.f, "a") as fd:
+            fd.write("Hola mundo\n"
+            "este es un archivo\n"
+            "de ejemplo\n")
+        
 if __name__ == "__main__":
     Main()
