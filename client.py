7ed48fbc4a101843694b66ba3ff72bd79a5506f8
Tu Nombre <you@example.com>
2022-09-02 19:14:11 -0300

    Arreglando push

--- client.py
+++ client.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-import click, socket, subprocess, threading
+import click, socket
 
 @click.command()
 @click.option("--h", help="Host.", prompt="Enter the host: ")
@@ -14,24 +13,7 @@ def main(h, p):
         s.send(com.encode())
         std = s.recv(1024)
         print(std.decode())
-=======
-import click
-import socket
 
-from prompt_toolkit import prompt
-
-@click.command()
-@click.option("--h", help="The host", prompt="Host: ")
-@click.option("--p", help="Port", prompt="Port: ")
-def main(h,p):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                           
-    #Completar para tagear
-    print("Connecting.")
-    s.connect((h, p))   
-    msg = s.recv(1024)                                     
-    s.close()
-    print("Cerrando conexion")
->>>>>>> 666ba753176518031b17211e9207267cd33b926e
 
 if __name__ == "__main__":
     main()
